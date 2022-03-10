37ef6643350adae016e767843b5fe28d1111a9ad
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-09 20:25:18 -0500

    Starting to model in pydantic

--- main.py
+++ main.py
@@ -8,57 +8,32 @@ import os
 import getpass
 from environs import *
 from login import LoginClass
-from trackclass import TrackClass
 
 
 def main():
     global tracks_slice
     user = LoginClass()
-    track_list = []
-    user_name_info = user.get_user_info()
-    user_name = user_name_info['display_name']
+    user_name = user.user_class.display_name
     print(f"\nPlaylists for user: {user_name}")
     user_playlists = user.get_playlists(playlist_limit="2", offset="0")
-    playlist_content = user_playlists['items']
-    # TODO: use recursion to get the full playlist track listing
-    for pl_item in playlist_content:
-        track_count_slice = pl_item['tracks']
-        track_count = track_count_slice['total']
+    for pl_item in user_playlists:
+        track_count = pl_item.tracks.total
         if track_count <= 20:
-            tracks_slice = user.get_playlist_tracks(pl_item['id'])
+            track_slice = user.get_playlist_tracks(pl_item.id)
         elif 20 < track_count <= 50:
-            tracks_slice = user.get_playlist_tracks(pl_item['id'], track_limit=str(track_count))
+            track_slice = user.get_playlist_tracks(pl_item.id, track_limit=str(track_count))
         elif track_count > 50:
             track_c_remaining = track_count
             while track_c_remaining > 0:
-                tracks_slice = user.get_playlist_tracks(pl_item['id'], track_limit=str(track_c_remaining))
-        print(f"\nPlaylist ID: {pl_item['id']} -- Name: {pl_item['name']} -- Track Count: {track_count}")
-        tracks = tracks_slice['items']
-        for track in tracks:
+                track_slice = user.get_playlist_tracks(pl_item.id, track_limit=str(track_c_remaining))
+        print(f"\nPlaylist ID: {pl_item.id} -- Name: {pl_item.name} -- Track Count: {track_count}")
+        for cur_track in track_slice:
             artist_list = []
-            track_slice = track['track']
-            artist_slice = track_slice['artists']
-            if len(artist_slice) >= 1:
-                for artist in artist_slice:
-                    artist_list.append(artist['name'])
-            else:
-                artist_list.append(track_slice['artists'])
-            if track_slice['album']['album_type'] == 'single':
-                track_list.append(TrackClass(name=track_slice['name'],
-                                             artist=artist_list,
-                                             release=track_slice['album']['release_date'],
-                                             album="",
-                                             is_single=True))
-                print('single')
-                print(track_list[len(track_list)-1].return_details())
-            else:
-                print('album')
-                track_list.append(TrackClass(name=track_slice['name'],
-                                             artist=artist_list,
-                                             release=track_slice['album']['release_date'],
-                                             album=track_slice['album']['name'],
-                                             is_single=False))
-                print(track_list[len(track_list)-1].return_details())
+            for artist in cur_track.track.artists:
+                artist_list.append(artist.name)
+            track_name = cur_track.track.name
+            album_name = cur_track.track.album.name
+            print(f"{artist_list} - {album_name} - {track_name}")
 
 
 if __name__ == "__main__":
--- playlistclass.py
+++ playlistclass.py
@@ -1,10 +1,62 @@
-from dataclasses import dataclass
+from __future__ import annotations
+from typing import Any, List, Optional
+from pydantic import BaseModel
+import ujson
 
 
-@dataclass(init=True)
-class PlaylistClass:
+class ExternalUrls(BaseModel):
+    spotify: str
+
+
+class Image(BaseModel):
+    height: int
+    url: str
+    width: int
+
+
+class ExternalUrls1(BaseModel):
+    spotify: str
+
+
+class Owner(BaseModel):
+    display_name: str
+    external_urls: ExternalUrls1
+    href: str
+    id: str
+    type: str
+    uri: str
+
+
+class Tracks(BaseModel):
+    href: str
+    total: int
+
+
+class Item(BaseModel):
+    collaborative: bool
+    description: str
+    external_urls: ExternalUrls
+    href: str
+    id: str
+    images: List[Image]
     name: str
-    length: int
+    owner: Owner
+    primary_color: Any
+    public: bool
+    snapshot_id: str
+    tracks: Tracks
+    type: str
+    uri: str
 
-    def return_details(self):
-        return self.name, self.length
+
+class PlaylistClass(BaseModel):
+    href: str
+    items: List[Item]
+    limit: int
+    next: str
+    offset: int
+    previous: Any
+    total: int
+
+    class Config:
+        json_loads = ujson.loads
