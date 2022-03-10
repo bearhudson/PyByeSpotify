8c21c1e4d9c620c9c17957e80ab5cf5a7a5883ac
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-09 21:57:57 -0500

    Trying to think about how to do long lists.

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ from login import LoginClass
 
 
 def main():
-    global tracks_slice
+    global track_slice
     user = LoginClass()
     user_name = user.user_class.display_name
     print(f"\nPlaylists for user: {user_name}")
-    user_playlists = user.get_playlists(playlist_limit="2", offset="0")
+    user_playlists = user.get_playlists(playlist_limit="50", offset="0")
     for pl_item in user_playlists:
         track_count = pl_item.tracks.total
-        if track_count <= 20:
-            track_slice = user.get_playlist_tracks(pl_item.id)
-        elif 20 < track_count <= 50:
-            track_slice = user.get_playlist_tracks(pl_item.id, track_limit=str(track_count))
-        elif track_count > 50:
-            track_c_remaining = track_count
-            while track_c_remaining > 0:
-                track_slice = user.get_playlist_tracks(pl_item.id, track_limit=str(track_c_remaining))
         print(f"\nPlaylist ID: {pl_item.id} -- Name: {pl_item.name} -- Track Count: {track_count}")
+        while track_count > 50:
+            track_position = int(track_count) - 50
+            offset = track_count - track_position
+            track_slice = user.get_playlist_tracks(pl_item.id, track_limit=track_position, offset=offset)
+            track_count -= track_position
+        else:
+            track_slice = user.get_playlist_tracks(pl_item.id, track_limit=str(track_count))
+
         for cur_track in track_slice:
             artist_list = []
             for artist in cur_track.track.artists:
