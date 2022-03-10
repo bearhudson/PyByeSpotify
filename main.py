32c1fb5aece5c8980ab37f0bc90912aa08815df7
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-10 07:46:52 -0500

    More progress

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from login import LoginClass
 
 
-def get_playlists_recursive(count, reps, user):
+def get_playlists_recursive(count, reps, user, playlists):
     try:
-        if count > 50:
-            get_playlists_recursive(count - 50, reps + 1, user)
-        return user.get_playlists()
+        if count > 20:
+            get_playlists_recursive(count - 20, reps + 1, user, playlists)
+        playlists += user.get_playlists(playlist_limit=count, offset=(reps * 20))
     except ValueError:
         print("Error")
-
+    return playlists
 
 def get_tracks_recursive(count, reps, user, playlist_id, track_slice):
     try:
@@ -24,12 +24,13 @@ def main():
     user = LoginClass()
     user_name = user.user_class.display_name
     print(f"\nPlaylists for user: {user_name}")
-    user_playlists = get_playlists_recursive(10, 40, user)
+    playlists = []
+    user_playlists = get_playlists_recursive(50, 0, user, playlists)
     for pl_item in user_playlists:
         track_slice = []
         track_count = pl_item.tracks.total
         track_slice = get_tracks_recursive(track_count, 0, user, playlist_id=pl_item.id, track_slice=track_slice)
-        print(f"\n\nPlaylist Name: {pl_item.name} -- Track Count: {track_count}")
+        print(f"\n\nPlaylist Name: {pl_item.name} -- Track Count: {track_count}\n--------------")
         for cur_track in track_slice:
             artist_list = []
             for artist in cur_track.track.artists:
