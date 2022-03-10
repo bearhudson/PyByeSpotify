5ff34ea0cc31c8fd3eb89388545b137e151b7fc8
Brian Hudson <reallybrianhudson@gmail.com>
2022-03-10 07:29:24 -0500

    Recursive playlist and track list creation

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-import requests
-import rich
-import json
-import base64
-import hashlib
-import html
-import os
-import getpass
-from environs import *
 from login import LoginClass
 
 
+def get_playlists_recursive(count, reps, user):
+    try:
+        if count > 50:
+            get_playlists_recursive(count - 50, reps + 1, user)
+        return user.get_playlists()
+    except ValueError:
+        print("Error")
+
+
+def get_tracks_recursive(count, reps, user, playlist_id, track_slice):
+    try:
+        if count > 50:
+            get_tracks_recursive(count - 50, reps + 1, user, playlist_id, track_slice)
+        track_slice += user.get_playlist_tracks(playlist_id=playlist_id, track_limit=count, offset=(reps * 50))
+    except ValueError:
+        print("Error")
+    return track_slice
+
+
 def main():
-    global track_slice
     user = LoginClass()
     user_name = user.user_class.display_name
     print(f"\nPlaylists for user: {user_name}")
-    user_playlists = user.get_playlists(playlist_limit="50", offset="0")
+    user_playlists = get_playlists_recursive(10, 40, user)
     for pl_item in user_playlists:
+        track_slice = []
         track_count = pl_item.tracks.total
-        print(f"\nPlaylist ID: {pl_item.id} -- Name: {pl_item.name} -- Track Count: {track_count}")
-        while track_count > 50:
-            track_position = int(track_count) - 50
-            offset = track_count - track_position
-            track_slice = user.get_playlist_tracks(pl_item.id, track_limit=track_position, offset=offset)
-            track_count -= track_position
-        else:
-            track_slice = user.get_playlist_tracks(pl_item.id, track_limit=str(track_count))
-
+        track_slice = get_tracks_recursive(track_count, 0, user, playlist_id=pl_item.id, track_slice=track_slice)
+        print(f"\n\nPlaylist Name: {pl_item.name} -- Track Count: {track_count}")
         for cur_track in track_slice:
             artist_list = []
             for artist in cur_track.track.artists:
